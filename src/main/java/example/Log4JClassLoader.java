b910c49d077d383ece6f4a01317945daf1a2c29a
Aman Sharma <mannu.poski10@gmail.com>
2023-12-07 20:28:42 +0100

    Remove if condition

--- src/main/java/example/Log4JClassLoader.java
+++ src/main/java/example/Log4JClassLoader.java
@@ -8,23 +8,19 @@ import java.security.SecureClassLoader;
 public class Log4JClassLoader extends SecureClassLoader {
 
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if (name.startsWith("org.apache.logging.log4j")) {
-            System.out.println("Loading class from " + Log4JClassLoader.class + name);
-            String classfile = name.replace('.', '/') + ".class";
+    public Class<?> loadClass(String name) {
+        System.out.println("Loading class from " + Log4JClassLoader.class + name);
+        String classfile = name.replace('.', '/') + ".class";
 
-            InputStream is = getResourceAsStream(classfile);
-            byte[] bytes;
-            try {
-                bytes = new byte[is.available()];
-                DataInputStream dis = new DataInputStream(is);
-                dis.readFully(bytes);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return defineClass(name, bytes, 0, bytes.length);
+        InputStream is = getResourceAsStream(classfile);
+        byte[] bytes;
+        try {
+            bytes = new byte[is.available()];
+            DataInputStream dis = new DataInputStream(is);
+            dis.readFully(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println("Loading class from " + Log4JClassLoader.super.getParent() + name);
-        return super.loadClass(name);
+        return defineClass(name, bytes, 0, bytes.length);
     }
 }
--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -5,7 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         // use custom classloader to load log4j classes
-        ClassLoader customClassLoader = new Log4JClassLoader();
+        Log4JClassLoader customClassLoader = new Log4JClassLoader();
         Class<?> clazz = customClassLoader.loadClass("example.ReflectiveLogging");
 
         clazz.getMethod("reflectiveLogging", String.class).invoke(null, "Hello, world!");
