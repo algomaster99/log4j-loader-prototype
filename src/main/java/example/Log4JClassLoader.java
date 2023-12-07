b2d3bf7181bdcb765c950921a425fa8b3896dd84
Aman Sharma <mannu.poski10@gmail.com>
2023-12-07 21:56:02 +0100

    Try setting permissions

--- src/main/java/example/Log4JClassLoader.java
+++ src/main/java/example/Log4JClassLoader.java
@@ -3,14 +3,19 @@ package example;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.Permissions;
+import java.security.ProtectionDomain;
 import java.security.SecureClassLoader;
+import java.security.cert.Certificate;
 
 public class Log4JClassLoader extends SecureClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
         if (name.startsWith("example") || name.startsWith("org.apache.logging.log4j.")) {
-            System.out.println("Loading class from " + Log4JClassLoader.class + name);
             String classfile = name.replace('.', '/') + ".class";
 
             InputStream is = getResourceAsStream(classfile);
@@ -22,10 +27,23 @@ public class Log4JClassLoader extends SecureClassLoader {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            Class<?> customLoadedClass = defineClass(name, bytes, 0, bytes.length);
+
+            Class<?> customLoadedClass = defineClass(name, bytes, 0, bytes.length, getProtectionDomain());
             resolveClass(customLoadedClass);
             return customLoadedClass;
         }
         return super.loadClass(name);
     }
+
+    private static ProtectionDomain getProtectionDomain() {
+        CodeSource codeSource;
+        try {
+            codeSource = new CodeSource(new URL("file://tmp/untitled1/target/classes/example/ReflectiveLogging.class"), (Certificate[]) null);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        Permissions permissions = new Permissions();
+        permissions.setReadOnly();
+        return new ProtectionDomain(codeSource, permissions);
+      }
 }
--- src/main/java/example/ReflectiveLogging.java
+++ src/main/java/example/ReflectiveLogging.java
@@ -3,8 +3,14 @@ package example;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ReflectiveLogging {
-    public static void reflectiveLogging(String message) {
+    public static void reflectiveLogging(String message) throws IOException {
         LogManager.getLogger().log(Level.ERROR, message);
+        String content = Files.readAllLines(Paths.get("a.txt")).get(0);
+        System.out.println(content);
     }
 }
